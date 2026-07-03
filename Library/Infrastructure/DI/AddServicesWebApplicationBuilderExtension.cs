e4f1fac185eab3c45e88ecdaae29c8c57364c345
yaroslav <yaroslav.zarytskyi.ki.2021@lpnu.ua>
2026-07-03 18:48:23 +0200

    git added DI exntensions

---
+++ Library/Infrastructure/DI/AddServicesWebApplicationBuilderExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.Infrastructure.DI;
+
+public static class AddServicesWebApplicationBuilderExtension
+{
+    public static WebApplicationBuilder AddServices(this WebApplicationBuilder builder)
+    {
+        var services = builder.Services;
+
+        return builder;
+    }
+}
