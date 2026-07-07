bc494c937cce0f21dff9b085a99768638cfa13b1
KingOfTyphoon13 <poseudonimus13@gmail.com>
2026-07-07 14:52:42 +0200

    Complitly implemented AuthorsRepositroty

--- Library.DAL/DataAccess/AuthorsRepository.cs
+++ Library.DAL/DataAccess/AuthorsRepository.cs
@@ -10,28 +10,98 @@ internal class AuthorsRepository : BaseRepository, IAuthorsRepository
     {
     }
 
-    public Task<int> AddAsync(AuthorDTO dto)
+    public async Task<int> AddAsync(AuthorDTO dto)
     {
-        throw new NotImplementedException();
+        const string query = @"
+        Insert into authors (name, surname) 
+        Output Inserted.id
+        Values(@Name, @Surname)";
+
+        var cmd = new SqlCommand(query, _connection, _transaction());
+        cmd.Parameters.Add(new SqlParameter("@Name", dto.Name));
+        cmd.Parameters.Add(new SqlParameter("@Surname", dto.Surname));
+
+        return (int)await cmd.ExecuteScalarAsync();
     }
 
-    public Task<bool> ExistsAsync(int id)
+    public async Task<bool> ExistsAsync(int id)
     {
-        throw new NotImplementedException();
+        const string sql = "SELECT 1 FROM authors WHERE id = @Id";
+
+        await _openDbConnectionAsync();
+
+        await using var command = new SqlCommand(sql, _connection, _transaction());
+        command.Parameters.Add(new SqlParameter("@Id", id));
+
+        var result = await command.ExecuteScalarAsync();
+        return result is not null;
     }
 
-    public Task<List<AuthorWithBooksCountDTO>> GetAllWithBookCountAsync()
+    public async Task<List<AuthorWithBooksCountDTO>> GetAllWithBookCountAsync()
     {
-        throw new NotImplementedException();
+        const string query = @"
+        SELECT a.id, a.name, a.surname,
+               Count(b.id) as books_count
+        FROM authors a
+        LEFT JOIN bookauthors ba ON ba.author_id = a.id
+        LEFT JOIN books b ON b.id = ba.book_id
+        GROUP BY a.id, a.name, a.surname
+        ORDER BY a.id";
+
+        var result = new List<AuthorWithBooksCountDTO>();
+
+        await _openDbConnectionAsync();
+
+        await using var command = new SqlCommand(query, _connection, _transaction());
+        await using var reader = await command.ExecuteReaderAsync();
+
+        var idOrd = reader.GetOrdinal("id");
+        var nameOrd = reader.GetOrdinal("name");
+        var surnameOrd = reader.GetOrdinal("surname");
+        var countOrd = reader.GetOrdinal("books_count");
+
+        while (await reader.ReadAsync())
+        {
+            result.Add(new AuthorWithBooksCountDTO
+            {
+                Id = reader.GetInt32(idOrd),
+                Name = reader.GetString(nameOrd),
+                Surname = reader.GetString(surnameOrd),
+                Count = reader.GetInt32(countOrd)
+            });
+        }
+
+        return result;
     }
 
-    public Task<List<AuthorDTO>> GetAuthors()
+    public async Task<List<AuthorDTO>> GetAuthors()
     {
-        throw new NotImplementedException();
-    }
+        const string sql = @"
+        SELECT a.id, a.name, a.surname
+        FROM authors a
+        ORDER BY a.id";
 
-    public Task<AuthorDTO?> GetByIdAsync(int id)
-    {
-        throw new NotImplementedException();
+        var result = new List<AuthorDTO>();
+
+        await _openDbConnectionAsync();
+
+        await using var command = new SqlCommand(sql, _connection, _transaction());
+        await using var reader = await command.ExecuteReaderAsync();
+
+        var idOrd = reader.GetOrdinal("id");
+        var nameOrd = reader.GetOrdinal("name");
+        var surnameOrd = reader.GetOrdinal("surname");
+
+        while (await reader.ReadAsync())
+        {
+            result.Add(new AuthorDTO
+            {
+                Id = reader.GetInt32(idOrd),
+                Name = reader.GetString(nameOrd),
+                Surname = reader.GetString(surnameOrd),
+            });
+        }
+
+        return result;
     }
 }
