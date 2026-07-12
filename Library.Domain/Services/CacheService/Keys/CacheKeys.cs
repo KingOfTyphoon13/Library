de9d2eb91eb1807bd45b4079b3f5bcc65f10aa52
KingOfTyphoon13 <poseudonimus13@gmail.com>
2026-07-12 21:05:07 +0200

    Temporery save

---
+++ Library.Domain/Services/CacheService/Keys/CacheKeys.cs
@@ -0,0 +1,45 @@
+﻿namespace Library.Domain.Services.CacheService.Keys;
+
+internal static class CacheKeys
+{
+    private const string Separator = ":";
+
+    private static string Build(params object[] parts) => string.Join(Separator, parts);
+
+    internal static class Authors
+    {
+        private const string VersionKey = "authors:version";
+
+        internal static string Version() => VersionKey;
+
+        internal static string Paged(long version, int pageNumber, int pageSize) =>
+            Build("authors", "v" + version, "paged", pageNumber, pageSize);
+
+        internal static string Keyset(long version, int? lastItemId, int pageSize) =>
+            Build("authors", "v" + version, "keyset", lastItemId?.ToString() ?? "start", pageSize);
+
+        internal static string ById(int id) => Build("authors", id);
+    }
+
+    internal static class Books
+    {
+        private const string VersionKey = "books:version";
+
+        internal static string Version() => VersionKey;
+
+        internal static string Paged(long version, int pageNumber, int pageSize) =>
+            Build("books", "v" + version, "paged", pageNumber, pageSize);
+
+        internal static string ById(int id) => Build("books", id);
+    }
+
+    internal static class Reviews
+    {
+        private const string VersionKey = "reviews:version";
+
+        internal static string Version() => VersionKey;
+
+        internal static string PagedByBook(long version, int bookId, int pageNumber, int pageSize) =>
+            Build("reviews", "v" + version, "book", bookId, "paged", pageNumber, pageSize);
+    }
+}
